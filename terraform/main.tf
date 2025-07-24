138ff7f1b62c1e084e8eb2d2a8809ac2e07cc58c
Vinicius Barbosa <vinicius-uct@hotmail.com.br>
2025-07-24 19:04:57 -0300

    Add terraform configuration for Azure resources & .dockerignore file

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {
+  default = "hello-rg"
+}
+
+variable "location" {
+  default = "East US 2"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "helloacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "hello-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "hello-world-devops"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|helloacr.azurecr.io/hello-world-app:v1"
+  }
+}
